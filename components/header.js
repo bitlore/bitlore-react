6ef1a2c8c2174f90e014bd62b79b4a4d365a3b22
ethan <ejlaw01@gmail.com>
2020-01-24 19:32:48 -0800

    bring back navigation to work section

--- components/header.js
+++ components/header.js
@@ -18,13 +18,11 @@ class Header extends React.Component {
             <a>about</a>
           </div>
         </NavLink>
-        {/*          
-          <NavLink activeClassName="on" href="/work">
+        <NavLink activeClassName="on" href="/work">
           <div>
-          <a>work</a>
+            <a>work</a>
           </div>
-          </NavLink>
-        */}
+        </NavLink>
         <NavLink activeClassName="on" href="/contact">
           <div>
             <a>contact</a>
